f0e1b4e1e2d75fd66b35ed1730d449267c2d5e4a
blade-demon <xuziwei89@gmail.com>
2019-09-19 01:05:10 +0800

    add ts for express

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
-import cookieSession from 'cookie-session';
+import cookieSession from "cookie-session";
 
-const app = express();
+class Server {
+	app: express.Express = express();
+	constructor() {
+		this.app.use(
+			bodyParser.urlencoded({
+				extended: true
+			})
+		);
+		this.app.use(cookieSession({ keys: ["express_ts"] }));
+		this.app.use(router);
+	}
+	start(): void {
+		this.app.listen(3000, () => {
+			console.log("Listening on port 3000");
+		});
+	}
+}
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(cookieSession({keys: ["express_ts"]}));
-app.use(router);
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+new Server().start();
